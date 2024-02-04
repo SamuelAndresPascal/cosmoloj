8f66ad5ad9fa9101d8620405fbd56586d0eabb6f
Samuel Andrés <samuelandres@yandex.com>
2024-02-05 00:06:57 +0100

    refactor(language:wkt:geom): prepare some keywords and module deps

---
+++ language-wkt-geom/src/main/java/com/cosmoloj/language/wkt/geom/Keyword.java
@@ -0,0 +1,47 @@
+package com.cosmoloj.language.wkt.geom;
+
+import com.cosmoloj.bibliography.cosmoloj.Cosmoloj;
+import com.cosmoloj.language.common.impl.builder.EnumLexemeBuilder;
+import com.cosmoloj.language.common.impl.semantic.EnumCase;
+import com.cosmoloj.language.common.impl.semantic.SemanticEnum;
+import com.cosmoloj.util.bib.Reference;
+
+/**
+ *
+ * @author Samuel Andrés
+ */
+@Reference(Cosmoloj.SF_ACCESS_PART_1_V1_2_1)
+public enum Keyword implements SemanticEnum<Keyword> {
+    POINT,
+    LINESTRING,
+    POLYGON,
+    POLYHEDRALSURFACE,
+    TRIANGLE,
+    TIN,
+    MULTIPOINT,
+    MULTILINESTRING,
+    MULTIPOLYGON,
+    GEOMETRYCOLLECTION;
+
+
+    private final String codePoints;
+
+    Keyword(final String codePoints) {
+        this.codePoints = codePoints;
+    }
+
+    Keyword() {
+        this(null);
+    }
+
+    @Override
+    public String getCodePoints() {
+        return this.codePoints == null ? name() : this.codePoints;
+    }
+
+
+
+    public static EnumLexemeBuilder<Keyword> builder() {
+        return EnumCase.IGNORE.builder(Keyword.class, Keyword.values());
+    }
+}
---
+++ language-wkt-geom/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.cosmoloj.language.wkt.geom {
+    requires com.cosmoloj.util;
+    requires com.cosmoloj.bibliography.cosmoloj;
+    requires com.cosmoloj.language.common;
+    exports com.cosmoloj.language.wkt.geom;
+}
